7361c4c5f44b4e2cb3d222c14837590a6683b4a5
TaiDang <tai.dt@kdc.vn>
2021-10-27 15:33:29 +0700

    Add course Algorithms Part I

---
+++ Assignment0/HelloGoodbye.java
@@ -0,0 +1,35 @@
+/******************************************************************************
+ *  Compilation:  javac HelloWorld.java
+ *  Execution:    java HelloWorld
+ *
+ *  Prints "Hello, World". By tradition, this is everyone's first program.
+ *
+ *  % java HelloWorld
+ *  Hello, World
+ *
+ *  These 17 lines of text are comments. They are not part of the program;
+ *  they serve to remind us about its properties. The first two lines tell
+ *  us what to type to compile and test the program. The next line describes
+ *  the purpose of the program. The next few lines give a sample execution
+ *  of the program and the resulting output. We will always include such 
+ *  lines in our programs and encourage you to do the same.
+ *
+ ******************************************************************************/
+
+public class HelloGoodbye {
+
+    public static void main(String[] args) {
+        // Prints "Hello and Goodbye" to the terminal window.
+        String name1 = "";
+        String name2 = "";
+        if (args.length >= 1) {
+            name1 = args[0];
+        }
+        if (args.length >= 2) {
+            name2 = args[1];
+        }
+        System.out.println("Hello " + name1 + " and " + name2 + ".");
+        System.out.println("Goodbye " + name2 + " and " + name1 + ".");
+    }
+
+}
---
+++ Assignment0/RandomWord.java
@@ -0,0 +1,41 @@
+/******************************************************************************
+ *  Compilation:  javac HelloWorld.java
+ *  Execution:    java HelloWorld
+ *
+ *  Prints "Hello, World". By tradition, this is everyone's first program.
+ *
+ *  % java HelloWorld
+ *  Hello, World
+ *
+ *  These 17 lines of text are comments. They are not part of the program;
+ *  they serve to remind us about its properties. The first two lines tell
+ *  us what to type to compile and test the program. The next line describes
+ *  the purpose of the program. The next few lines give a sample execution
+ *  of the program and the resulting output. We will always include such 
+ *  lines in our programs and encourage you to do the same.
+ *
+ ******************************************************************************/
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+
+    public static void main(String[] args) {
+        int i = 1;
+        String champion = "";
+        while (true) {
+            if (StdIn.isEmpty()) {
+                StdOut.println(champion);
+                return;
+            }
+            String str = StdIn.readString();
+            if (StdRandom.bernoulli((double) 1 / i)) {
+                champion = str;
+            }
+            i = i + 1;
+        }
+
+    }
+
+}
